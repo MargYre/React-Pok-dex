71b95911d2cebe5c4d3fa2f8b8878396f8902fd1
Tarayre Margaux <tmargaux@pc-166.home>
2024-01-05 19:39:21 +0100

    Add librairy materialize to change the display of my site

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
-import React, { FunctionComponent, useState} from 'react';
+import React, { FunctionComponent, useState, useEffect} from 'react';
 import Pokemon from './models/pokemon';
 import POKEMONS from './models/mock-pokemon'
 
 const App: FunctionComponent = () => {
- const [pokemons] = useState<Pokemon[]>(POKEMONS);
-    
+ //const [pokemons] = useState<Pokemon[]>(POKEMONS);
+ const [pokemons, setPokemons] = useState<Pokemon[]>([]);
+
+ useEffect(() => {
+    setPokemons(POKEMONS);
+ }, []);
+
  return (
 <div>
   <h1>Pokédex</h1>
     <p>Il y a {pokemons.length} pokémon dans le pokédex</p>
+    <p>plop</p>
   </div>
  )
 }
