d7f55f6677c286f29f4614a7ddee84c97b668948
Tarayre Margaux <tmargaux@pc-166.home>
2024-01-06 02:01:48 +0100

    Creation of the componant PokemonList, and display information in App.tsx from it

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,10 @@
-import React, { FunctionComponent, useState, useEffect} from 'react';
-import Pokemon from './models/pokemon';
-import POKEMONS from './models/mock-pokemon'
+import React, { FunctionComponent } from 'react';
+import PokemonList from './pages/pokemon-list';
 
 const App: FunctionComponent = () => {
- //const [pokemons] = useState<Pokemon[]>(POKEMONS);
- const [pokemons, setPokemons] = useState<Pokemon[]>([]);
-
- useEffect(() => {
-    setPokemons(POKEMONS);
- }, []);
 
  return (
-<div>
-  <h1>Pokédex</h1>
-    <p>Il y a {pokemons.length} pokémon dans le pokédex</p>
-    
-    <p>plop</p>
-    <ul>
-      {pokemons.map((pokemon) => (
-        <li key={pokemon.id}>{pokemon.name}</li>
-      ))}
-    </ul>
-  </div>
+  <PokemonList/>
  )
 }
   
