261d012a45bef0c460929a39b0ead4c123c2ed50
Tarayre Margaux <tmargaux@pc-166.home>
2024-01-17 01:31:46 +0100

    Add formulaire to change pokemon's data

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import PokemonList from './pages/pokemon-list';
 import PokemonsDetail from './pages/pokemon-detail';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import PageNotFound from './pages/page-not-found';
+import PokemonEdit from './pages/pokemon-edit';
 //import { Link } from 'react-router-dom';
 
 const App: FunctionComponent = () => {
@@ -20,6 +21,7 @@ const App: FunctionComponent = () => {
         <Switch>
             <Route exact path="/" component={PokemonList}/>
             <Route exact path="/pokemons" component={PokemonList}/>
+            <Route exact path="/pokemons/edit/:id" component={PokemonEdit}/>
             <Route path="/pokemons/:id" component={PokemonsDetail}/>
             <Route component={PageNotFound}/>
         </Switch>
